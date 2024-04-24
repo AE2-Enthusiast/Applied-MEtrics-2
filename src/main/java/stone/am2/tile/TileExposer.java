a89d46c44970a02060785244791e912eed9cdbaa
stone <samstone9001@gmail.com>
2024-04-24 11:25:50 -0400

    Use unlocalized name as label (bad)

--- src/main/java/stone/am2/tile/TileExposer.java
+++ src/main/java/stone/am2/tile/TileExposer.java
@@ -9,6 +9,7 @@ import appeng.api.networking.events.MENetworkStorageEvent;
 import appeng.api.storage.IStorageChannel;
 import appeng.api.storage.channels.IFluidStorageChannel;
 import appeng.api.storage.channels.IItemStorageChannel;
+import appeng.api.storage.data.IAEFluidStack;
 import appeng.api.storage.data.IAEItemStack;
 import appeng.api.storage.data.IItemList;
 import appeng.tile.grid.AENetworkTile;
@@ -16,34 +17,62 @@ import io.prometheus.metrics.core.datapoints.GaugeDataPoint;
 import io.prometheus.metrics.core.metrics.Gauge;
 import io.prometheus.metrics.model.snapshots.Unit;
 import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
 
 public class TileExposer extends AENetworkTile {
 
-	private static Map<String, Map<Object, Map<Integer, GaugeDataPoint>>> datapoints = new HashMap<>();
+	private static Map<Item, Map<Integer, GaugeDataPoint>> itemDatapoints = new HashMap<>();
+	private static Map<Fluid, GaugeDataPoint> fluidDatapoints = new HashMap<>();
 	private static Map<String, Gauge> gauges = new HashMap<>();
+
+	private long lastTimeStamp = -1;
 
 	@MENetworkEventSubscribe
 	public void onStorageEvent(MENetworkStorageEvent event) {
+		long current = System.currentTimeMillis();
+		if (lastTimeStamp + 10000 < current) {
+			lastTimeStamp = current;
+		} else {
+			return;
+		}
 		String channelStr = channel2String(event.channel);
 		if (channelStr == null)
 			return;
-		Map<Object, Map<Integer, GaugeDataPoint>> gaugeMap = datapoints.computeIfAbsent(channelStr, (channel) -> {
-			return new HashMap<Object, Map<Integer, GaugeDataPoint>>();
-		});
 
 		switch (channelStr) {
 		case "item":
-			handleItems(gaugeMap, event.monitor.getStorageList());
+			handleItems(itemDatapoints, event.monitor.getStorageList());
+			break;
+		case "fluid":
+			handleFluids(fluidDatapoints, event.monitor.getStorageList());
 			break;
 		}
 	}
 
-	private void handleItems(Map<Object, Map<Integer, GaugeDataPoint>> itemMap, IItemList<IAEItemStack> storageList) {
+	private void handleFluids(Map<Fluid, GaugeDataPoint> fluidMap, IItemList<IAEFluidStack> storageList) {
+		for (IAEFluidStack stack : storageList) {
+			if (stack.isMeaningful()) {
+				Fluid fluidType = stack.getFluid();
+				GaugeDataPoint datapoint = fluidMap.computeIfAbsent(fluidType, fluid -> {
+					Gauge gauge = gauges.computeIfAbsent("fluid", channel -> {
+						return Gauge.builder().name("AE2_fluid_count").help("The counts of each fluid in the ME system")
+								.labelNames("unlocalized").unit(new Unit("milliBuckets")).register();
+					});
+					return gauge.labelValues(fluidType.getUnlocalizedName());
+				});
+
+				datapoint.set(stack.getStackSize());
+			}
+		}
+	}
+
+	private void handleItems(Map<Item, Map<Integer, GaugeDataPoint>> itemMap, IItemList<IAEItemStack> storageList) {
 		for (IAEItemStack stack : storageList) {
 			if (stack.isMeaningful()) {
 				Item itemType = stack.getItem();
 				if (itemType.isDamageable())
+					continue;
+				if (stack.hasTagCompound())
 					continue;
 				int meta = stack.getItemDamage();
 				GaugeDataPoint datapoint = itemMap.computeIfAbsent(itemType, item -> {
@@ -51,10 +80,13 @@ public class TileExposer extends AENetworkTile {
 				}).computeIfAbsent(meta, m -> {
 					Gauge gauge = gauges.computeIfAbsent("item", channel -> {
 						return Gauge.builder().name("AE2_item_count").help("The counts of each item in the ME system")
-								.labelNames("modid", "id", "meta").unit(new Unit("items")).register();
+								.labelNames("unlocalized").unit(new Unit("items")).register();
 					});
-					ResourceLocation name = itemType.getRegistryName();
-					return gauge.labelValues(name.getNamespace(), name.getPath(), String.valueOf(m));
+					try {
+						return gauge.labelValues(itemType.getTranslationKey(stack.createItemStack()));
+					} catch (Exception e) {
+						return gauge.labelValues(itemType.getTranslationKey());
+					}
 				});
 
 				datapoint.set(stack.getStackSize());
@@ -68,17 +100,6 @@ public class TileExposer extends AENetworkTile {
 		} else if (channel == AEApi.instance().storage().getStorageChannel(IFluidStorageChannel.class)) {
 			return "fluid";
 		} else {
-			return null;
-		}
-	}
-
-	private static String channel2Unit(String str) {
-		switch (str) {
-		case "item":
-			return "items";
-		case "fluid":
-			return "milliBuckets";
-		default:
 			return null;
 		}
 	}
